be5b7631fe4f9b9a47a65fec32ffc22515035274
Sergei Potekhin <vexilurz@gmail.com>
2021-08-31 13:24:57 +0300

    Using HttpFoundation from symfony

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-  $name = $_GET['name'] ?? 'World';
-  header('Content-Type: text/html; charset=utf-8');
-  printf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));
+  require_once __DIR__.'/vendor/autoload.php';
+
+  use Symfony\Component\HttpFoundation\Request;
+  use Symfony\Component\HttpFoundation\Response;
+
+  $request = Request::createFromGlobals();
+
+  $name = $request->get('name', 'World');
+
+  $response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+  $response->send();
