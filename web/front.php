e77e9b181b453850b0505532f8f026b4e4d6c1d5
Sergei Potekhin <vexilurz@gmail.com>
2021-08-31 14:44:17 +0300

    Refactor

--- web/front.php
+++ web/front.php
@@ -1,27 +1,24 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
-$response = new Response();
-
-const PAGES = "/../src/pages/";
 
 $map = [
-  '/hello' => __DIR__ . PAGES . 'hello.php',
-  '/bye'   => __DIR__ . PAGES . 'bye.php',
+  '/hello' => 'hello',
+  '/bye'   => 'bye',
 ];
 
 $path = $request->getPathInfo();
 if (isset($map[$path])) {
   ob_start();
-  include $map[$path];
-  $response->setContent(ob_get_clean());
+  extract($request->query->all(), EXTR_SKIP);
+  include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+  $response = new Response(ob_get_clean());
 } else {
-  $response->setStatusCode(404);
-  $response->setContent('Not Found');
+  $response = new Response('Not Found', 404);
 }
 
 $response->send();
