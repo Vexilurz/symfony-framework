7d55b1bbd7f0bb9d3e60baca167e68437adb47e3
Sergei Potekhin <vexilurz@gmail.com>
2021-08-31 13:48:39 +0300

    Refactor

--- web/front.php
+++ web/front.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -7,9 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 $request = Request::createFromGlobals();
 $response = new Response();
 
+const PAGES = "/../src/pages/";
+
 $map = [
-  '/hello' => __DIR__.'/hello.php',
-  '/bye'   => __DIR__.'/bye.php',
+  '/hello' => __DIR__ . PAGES . 'hello.php',
+  '/bye'   => __DIR__ . PAGES . 'bye.php',
 ];
 
 $path = $request->getPathInfo();
