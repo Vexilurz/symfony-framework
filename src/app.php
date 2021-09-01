bfac14a46072ba35108053813f3674da19f6b61f
Sergei Potekhin <vexilurz@gmail.com>
2021-09-01 12:57:03 +0300

    Self: include hello.php with controller

--- src/app.php
+++ src/app.php
@@ -6,5 +6,9 @@ $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
   'year' => null,
   '_controller' => 'Calendar\Controller\LeapYearController::index',
 ]));
+$routes->add('hello', new Routing\Route('/hello/{name}', [
+  'name' => 'World',
+  '_controller' => 'Page\Render\Controller\PageRenderController::index',
+]));
 
 return $routes;
--- web/index.php
+++ web/index.php
@@ -7,15 +7,6 @@ use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 
-//function render_template($request)
-//{
-//  extract($request->attributes->all(), EXTR_SKIP);
-//  ob_start();
-//  include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-//
-//  return new Response(ob_get_clean());
-//}
-
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
