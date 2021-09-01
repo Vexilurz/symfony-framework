59390b93ac5203e61d9219eb61f9dc1942a3dae6
Sergei Potekhin <vexilurz@gmail.com>
2021-09-01 16:25:47 +0300

    DependencyInjection container

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -1,56 +1,8 @@
 <?php
 namespace Simplex;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
-use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpKernel\HttpKernel;
 
 class Framework extends HttpKernel
 {
-  protected $dispatcher;
-  private $matcher;
-  private $controllerResolver;
-  private $argumentResolver;
-
-  public function __construct(EventDispatcher $dispatcher,
-                              UrlMatcherInterface  $matcher,
-                              ControllerResolverInterface $controllerResolver,
-                              ArgumentResolverInterface $argumentResolver)
-  {
-    $this->dispatcher = $dispatcher;
-    $this->matcher = $matcher;
-    $this->controllerResolver = $controllerResolver;
-    $this->argumentResolver = $argumentResolver;
-  }
-
-  public function handle(Request $request,
-                         $type = HttpKernelInterface::MAIN_REQUEST,
-                         $catch = true)
-  {
-    $this->matcher->getContext()->fromRequest($request);
-
-    try {
-      $request->attributes->add($this->matcher->match($request->getPathInfo()));
-
-      $controller = $this->controllerResolver->getController($request);
-      $arguments = $this->argumentResolver->getArguments($request, $controller);
-
-      $response = call_user_func_array($controller, $arguments);
-    } catch (ResourceNotFoundException $exception) {
-      $response = new Response('Not Found', 404);
-    } catch (Exception $exception) {
-      $response = new Response('An error occurred: '.$exception->getMessage(), 500);
-    }
-
-    // dispatch a response event
-    $this->dispatcher->dispatch(new ResponseEvent($response, $request), 'response');
-
-    return $response;
-  }
 }
--- web/index.php
+++ web/index.php
@@ -2,42 +2,12 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing;
 
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel;
+$routes = include __DIR__.'/../src/app.php';
+$container = include __DIR__.'/../src/container.php';
 
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
-$routes = include __DIR__.'/../src/app.php';
 
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-
-use Symfony\Component\EventDispatcher\EventDispatcher;
-
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
-//$dispatcher->addSubscriber(new Simplex\ContentLengthListener());
-//$dispatcher->addSubscriber(new Simplex\GoogleListener());
-$listener = new HttpKernel\EventListener\ErrorListener(
-  'Calendar\Controller\ErrorController::exception'
-);
-$dispatcher->addSubscriber($listener);
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
-
-$framework = new Simplex\Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
-//$framework = new HttpKernel\HttpCache\HttpCache(
-//  $framework,
-//  new HttpKernel\HttpCache\Store(__DIR__.'/../cache')
-//);
-$response = $framework->handle($request);
+$response = $container->get('framework')->handle($request);
 
 $response->send();
